1c07fc2ce9f32ba5025645635f0e2adbf6617d90
heeyeon-lim <heeyeon9916@gmail.com>
2023-05-20 15:40:06 +0900

    init: initialize project

---
+++ expenses-chart-component/src/styles/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    font-size: 18px;
+    font-family: 'DM Sans', sans-serif;
+}
+`;
+
+export default GlobalStyle;
---
+++ expenses-chart-component/src/styles/theme.js
@@ -0,0 +1,30 @@
+const windowSize = {
+  mobile: "screen and (max-width: 375px)",
+  desktop: "screen and (max-width: 1440px)",
+};
+
+const primaryColors = {
+  softRed: "hsl(10, 79%, 65%)",
+  cyan: "hsl(186, 34%, 60%)",
+};
+
+const neutralColors = {
+  darkBrown: "hsl(25, 47%, 15%)",
+  mediumBrown: " hsl(28, 10%, 53%)",
+  cream: "hsl(27, 66%, 92%)",
+  paleOrange: "hsl(33, 100%, 98%)",
+};
+
+const fontWeight = {
+  base: "400",
+  bold: "700",
+};
+
+const theme = {
+  windowSize,
+  primaryColors,
+  neutralColors,
+  fontWeight,
+};
+
+export default theme;
